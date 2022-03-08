a1f8ccc02591de0ba24d0c7d5397dd39bd40da9c
Dane Santos <dsantos@umass.edu>
2022-03-08 12:33:55 -0500

    added bootstrap theming with global styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Login from './components/pages/login'
 import './App.css';
+import './styles/custom.scss'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
--- client/src/components/pages/login.js
+++ client/src/components/pages/login.js
@@ -4,14 +4,14 @@ import '../../styles/Login.css'
 export default class login extends Component {
     render() {
         return (
-        <div className="login-background primaryColorBackground">
+        <div className="login-background bg-primary">
             <div className="form-signin">
             <form className="wrapperLogin">
                 <div className='login-logo-title d-flex mb-2'>
                     <img className="mt-1" src='/ukg_logo.svg' alt="UKG-LOGO" width={72} height={57} />
-                    <h2 className='mt-3 ml-2 fw-bold primaryColorText'>Inbox</h2>
+                    <h2 className='mt-3 ml-2 fw-bold text-primary'>Inbox</h2>
                 </div>
-                <h3 className="mb-3 fw-bold primaryColorText">Login</h3>
+                <h3 className="mb-3 fw-bold text-primary">Login</h3>
                 <div className="form-floating mb-3">
                     <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
                     <label htmlFor="floatingInput">Email address</label>
@@ -20,7 +20,7 @@ export default class login extends Component {
                     <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
                     <label htmlFor="floatingPassword">Password</label>
                 </div>
-                <a href="#" className="primaryColorText font-weight-bold mb-4 mt-2 text-decoration-none">Forgot Password</a>
+                <a href="#" className="text-primary font-weight-bold mb-4 mt-2 text-decoration-none">Forgot Password</a>
                 <button className="pl-4 pr-4 btn btn-med btn-primary rounded-pill" type="submit">Login</button>
             </form>     
             </div>
