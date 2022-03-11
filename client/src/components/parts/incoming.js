5cfd058a18bf7a29779d85842b1413272d80d7ed
Greg Garber <ggarber@umass.edu>
2022-03-10 20:05:17 -0500

    spacing and sizing for task reels - 2 per page

--- client/src/components/parts/incoming.js
+++ client/src/components/parts/incoming.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import TaskReel from "../parts/taskReel";
+import '../../styles/incoming.css'
 
 const pendingTaskReelURL = 'performanceReview/incoming/pending';
 const inProgressTaskReelURL = 'performanceReview/incoming/inProgress';
@@ -7,11 +8,14 @@ const completedTaskReelURL = 'performanceReview/incoming/completed';
 
 const Incoming = (props) => {
   return (
-    <div className="incoming d-flex bg-dark m-2">
+    <div className="incoming d-flex bg-dark ml-3 mr-3 mb-2 mt-2 flex-column justify-content-center">
+      <h1 className="text-white ml-5 pt-1">Incoming</h1>
       {/* these endpoints are passed to components where we will fetch data from the API later on*/}
-      <TaskReel name={props.reelItems[0]} endpoint={pendingTaskReelURL} />
-      <TaskReel name={props.reelItems[1]} endpoint={inProgressTaskReelURL} />
-      <TaskReel name={props.reelItems[2]} endpoint={completedTaskReelURL} />
+      <div className='task-reel-container d-flex bg-dark m-2 align-items-end justify-content-center h-95'>
+        <TaskReel name={props.reelItems[0]} endpoint={pendingTaskReelURL} />
+        <TaskReel name={props.reelItems[1]} endpoint={inProgressTaskReelURL} />
+        <TaskReel name={props.reelItems[2]} endpoint={completedTaskReelURL} />
+      </div>
     </div>
   )
 }
--- client/src/components/parts/outgoing.js
+++ client/src/components/parts/outgoing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TaskReel from "../parts/taskReel";
+import '../../styles/outgoing.css'
 
 const pendingTaskReelURL = 'performanceReview/outgoing/pending';
 const inProgressTaskReelURL = 'performanceReview/outgoing/inProgress';
@@ -7,11 +8,14 @@ const completedTaskReelURL = 'performanceReview/outgoing/completed';
 
 const Outgoing = (props) => {
     return (
-        <div className='outgoing d-flex bg-dark m-2'>
+        <div className='outgoing d-flex bg-dark ml-3 mr-3 mb-2 mt-2 flex-column justify-content-center'>
+            <h1 className="text-white ml-5 pt-1">Outgoing</h1>
             {/* these endpoints are passed to components where we will fetch data from the API later on*/}
+            <div className='task-reel-container d-flex bg-dark m-2 align-items-end justify-content-center h-95'>
             <TaskReel name={props.reelItems[0]} endpoint={pendingTaskReelURL} />
             <TaskReel name={props.reelItems[1]} endpoint={inProgressTaskReelURL} />
             <TaskReel name={props.reelItems[2]} endpoint={completedTaskReelURL} />
+            </div>
         </div>
     )
 }
