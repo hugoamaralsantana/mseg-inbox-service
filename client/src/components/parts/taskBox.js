75162b1203a715d6a36c9b3338d56c72cb8b92e9
Greg Garber <ggarber@umass.edu>
2022-03-21 15:12:13 -0400

    cards

--- client/src/components/parts/taskBox.js
+++ client/src/components/parts/taskBox.js
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
+import '../../styles/taskbox.css'
 
-const TaskBox = () => {
+const TaskBox = (props) => {
   return (
-    <div>
-        Task
+    <div className="task-box bg-secondary d-flex justify-content-between m-2">
+        <div className="left-side pl-2">
+          <h6 className="mb-0 lead address">From: Marius Minea</h6>
+          <p className="mb-0 date">Recieved: 04/20/2020</p>
+          <h7 className="start">Start</h7>
+        </div>
+        <div className="right-side">
+        </div>
     </div>
   )
 }
