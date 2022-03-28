640024de48d820101e5e0898f78bd0c1e239e5d3
Ali Rabeea <aaa.rabeea1411@gmail.com>
2022-03-27 20:28:35 -0400

    finished assign training and pto request pages, also fixed small bug with styling"

--- client/src/components/pages/PTORequest.js
+++ client/src/components/pages/PTORequest.js
@@ -1,9 +1,30 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import SideBar from "../parts/sidebar";
+import NavBar from "../parts/navbar";
+import PTORequest from "../parts/ptoRequest"
 
+const PTORequestPage = (props) => {
+  const [expanded, updateState] = useState(true);
+  const reelItems = ['Pending', 'In Progress', 'Completed']
 
-const PTORequest = () => {
+  function expandSideBar() {
+    if (expanded) {
+      updateState(false);
+  }
+  else {
+      updateState(true);
+  }
+  }
   return (
-    <h1>PTORequest</h1>
+    <div>
+      <NavBar title="PTO Request" /> 
+      <div className="d-flex">
+        <SideBar expandSideBar={expandSideBar} expanded={expanded}/>
+        <PTORequest reelItems={reelItems} expanded={expanded}/>
+      </div>
+    </div>
   )
 }
 
-export default PTORequest;
+export default PTORequestPage;
